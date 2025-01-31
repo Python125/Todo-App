0e243ab00e27a5f80c987c3dba9d2dfdf7e14bf0
PythonMonty <andrewnovosel90@gmail.com>
2025-01-31 14:18:28 -0500

    Adding 3 lists for each user

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, React } from 'react';
 import './App.css';
 import Login from './components/Login.jsx';
+import IncompleteList from './components/IncompleteList.jsx';
 
 function App() {
 
---
+++ src/components/IncompleteList.jsx
@@ -0,0 +1,14 @@
+import { useState, React } from 'react';
+
+function IncompleteList() {
+    return (
+        <div>
+            <h1>Incomplete List</h1>
+            <ul>
+                <li>Test</li>
+            </ul>
+        </div>
+    )
+}
+
+export default IncompleteList;
