ef6eb7a1356f9f90e542f2ac862f1dde569dfde5
PythonMonty <andrewnovosel90@gmail.com>
2025-02-07 07:20:05 -0500

    Currently building incomplete list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState, React, useEffect } from 'react';
 // import axios from 'axios';
 
@@ -6,6 +7,74 @@ function TodoList() {
     const [todoInput, setTodoInput] = useState('');
     const [editTodoId, setEditTodoId] = useState(null);
     const [dueDate, setDueDate] = useState('');
+    const [calenderDate, setCalenderDate] = useState(null);
+
+    useEffect(() => {
+        const fetchTodos = () => {
+            const response = axios.get(`${apiURL}/users/${userId}/todos`);
+            const updatedTodos = response.data.map(todo => {
+                if (!todo.completed && new Date(todo.dueDate) < new Date()) {
+                    return { ...todo, overdue: true };
+                } else {
+                    return { ...todo, overdue: false };
+                }
+            });
+            setTodos(updatedTodos);
+        };
+        fetchTodos();
+
+        const interval = setInterval(() => {
+            fetchTodos();
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    function addTodo(e) {
+        setTodoInput(e.target.value);
+    }
+
+    function addDueDate(e) {
+        setDueDate(e.target.value);
+    }
+
+    function submitTodo(e) {
+        e.preventDefault();
+        if (!todoInput.trim()) return;
+
+        const dateTime = new Date(calenderDate);
+        console.log(dateTime);
+
+        const newTodo = {
+            id: todos.length + 1,
+            name: todoInput,
+            completed: false,
+            dueDate: dueDate,
+            overdue: false,
+            userId: userId,
+        }
+        console.log(newTodo);
+
+        axios.post(`${apiURL}/users/${userId}/todos`, newTodo).then(response => {
+            setTodos([...todos, response.data]);
+            setTodoInput('');
+            setDueDate('');
+            setCalenderDate(null);
+        })
+    }
+
+    const updateTodo = (id, name, dueDate) => {
+        const newTodos = [...todos].map(todo => {
+            if (todo.id === id) {
+                return { ...todo, name: name, dueDate: dueDate };
+            } else {
+                return todo;
+            }
+        })
+
+        axios.put(`${apiURL}/users/${userId}/todos/${id}`, newTodos).then(response => {})
+    }
+
 
     return (
         <div>
@@ -14,7 +83,7 @@ function TodoList() {
                 <li>Incomplete Todo</li>
             </ul>
 
-            <h1>Complete</h1>
+            {/* <h1>Complete</h1>
             <ul>
                 <li>Completed Todo</li>
             </ul>
@@ -22,7 +91,7 @@ function TodoList() {
             <h1>Overdue</h1>
             <ul>
                 <li>Overdue Todo</li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
